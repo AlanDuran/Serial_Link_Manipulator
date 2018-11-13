fa1fe9f92f917811f23679e6f750e6e94d80735b
AlanDuran <ie704239@iteso.mx>
2018-11-12 21:56:37 -0600

    Cinematica directa, diferencial y control de velocidades

---
+++ D_H.m
@@ -0,0 +1,19 @@
+function T = D_H(q,a,alpha,d,inicio,fin)
+
+T = cell(1,fin);
+
+T{1,1} = [cos(q(1)) -cos(alpha(1))*sin(q(1)) sin(alpha(1))*sin(q(1)) a(1)*cos(q(1));...
+    sin(q(1)) cos(alpha(1))*cos(q(1)) -sin(alpha(1))*cos(q(1)) a(1)*sin(q(1));...
+    0 sin(alpha(1)) cos(alpha(1)) d(1);...
+    0 0 0 1];
+
+inicio = inicio + 2;
+
+for i = inicio:fin
+    T{1,i} = T{1,i-1}*[cos(q(i)) -cos(alpha(i))*sin(q(i)) sin(alpha(i))*sin(q(i)) a(i)*cos(q(i));...
+    sin(q(i)) cos(alpha(i))*cos(q(i)) -sin(alpha(i))*cos(q(i)) a(i)*sin(q(i));...
+    0 sin(alpha(i)) cos(alpha(i)) d(i);...
+    0 0 0 1];
+end
+
+end
